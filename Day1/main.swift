03b2625b116bc54372abe2b2e74a4062428b5ba3
Udhay Mahajan <udaymahajan98@gmail.com>
2019-06-06 21:01:54 -0400

    [Udhay] first commit

---
+++ Day1/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  Day1
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+var a=10
+var b=3
+
+func add()-> Int{
+    return a+b;
+}
+func sub()-> Int{
+    return a-b;
+}
+func mul()-> Int{
+    return a*b;
+}
+func div()-> Int{
+    return a/b;
+}
+func rem()-> Int{
+    return a%b;
+}
+
+print(add());
+print(sub());
+print(mul());
+print(div());
+print(rem());
